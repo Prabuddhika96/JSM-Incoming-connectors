2250c94992ed518b1fed7657f9f3b24de8a4a29e
prabuddhika.m <prabuddhika.m@eyepax.com>
2024-07-29 17:51:02 +0530

    Prepare incoming package

--- src/IncomingTrackingSystemFactory.ts
+++ src/IncomingTrackingSystemFactory.ts
@@ -1,12 +1,15 @@
+import { ITrackingSystemIncomingIssueHandler } from "interfaces/dist/ITrackingSystemIncomingIssueHandler";
 import { ClickupIncomingIssueHandler } from "./Handlers/ClickupIncomingIssueHandler";
 
 export class IncomingTrackingSystemFactory {
-  getHandler(systemIdentifier: string) {
+  getHandler(
+    systemIdentifier: string
+  ): ITrackingSystemIncomingIssueHandler | undefined {
     switch (systemIdentifier) {
       case "clickup":
         return new ClickupIncomingIssueHandler();
       default:
-        return null;
+        return undefined;
     }
   }
 }

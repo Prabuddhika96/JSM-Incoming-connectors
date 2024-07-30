566e2add9b55450599313c6ff56154852360d281
prabuddhika.m <prabuddhika.m@eyepax.com>
2024-07-30 10:17:58 +0530

    add build

--- dist/Handlers/ClickupIncomingIssueHandler.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from "express";
-import { IClientTransformedIssue } from "interfaces/dist/IClientTransformedIssue";
-import { ITrackingSystemIncomingIssueHandler } from "../../node_modules/interfaces/dist/ITrackingSystemIncomingIssueHandler";
-export declare class ClickupIncomingIssueHandler implements ITrackingSystemIncomingIssueHandler {
-    handleIncomingIssue(req: Request): IClientTransformedIssue;
-}
-//# sourceMappingURL=ClickupIncomingIssueHandler.d.ts.map
